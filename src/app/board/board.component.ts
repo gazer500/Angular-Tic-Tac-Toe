4aa9a75c7e5ed7a4091704ace4fdfe9705e5c566
gazer500 <yassineabbassi69@gmail.com>
2024-02-20 21:50:30 -0500

    somewhat working game

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SquareComponent } from "../square/square.component";
+
+@Component({
+    selector: 'app-board',
+    standalone: true,
+    templateUrl: './board.component.html',
+    styleUrl: './board.component.css',
+    imports: [CommonModule, SquareComponent]
+})
+export class BoardComponent implements OnInit {
+  squares: any[] = [];
+  xIsNext: boolean = true;
+  winner: string = "";
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+      this.newGame()
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.xIsNext = true;
+    this.winner = "";
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(i: number) {
+    if (!this.squares[i]) {
+      this.squares.splice(i, 1, this.player)
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-square',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>square works!</p>`,
+  template: `
+    <button>{{ value }}</button>
+  `,
   styles: ``
 })
-export class SquareComponent {}
+export class SquareComponent {
+  @Input() value: 'X' | 'O' = 'X';
+}
